bdc537891c17a9ada1e3e4ab8fe83103d07e6a70
Shen Han Wen <shenhan@teach.cs.toronto.edu>
2017-02-11 23:50:43 -0500

    working stats file

--- 401A1/stats.py
+++ 401A1/stats.py
@@ -10,20 +10,20 @@ def read_results(input_text):
     b = []
     for line in lines:
         line = line.rstrip("\n")
-        a_val = re.findall(r"([0-9]+  [0-9]+ \|   a = pos)", line)
+        a_val = re.findall(r"([0-9]+ +[0-9]+ \| +a = pos)", line)
         if len(a_val) > 0:
             a_list = re.findall(r"[0-9]+", a_val[0])
             a_list = [int(n) for n in a_list]
-            print a_list
             a.append(a_list)
-        b_val = re.findall(r"([0-9]+  [0-9]+ \|   b = neg)", line)
+        b_val = re.findall(r"([0-9]+ +[0-9]+ \| +b = neg)", line)
         if len(b_val) > 0:
             b_list = re.findall(r"[0-9]+", b_val[0])
             b_list = [int(n) for n in b_list]
             b.append(b_list)
-            print b_list
 
-    for i in range(len(a)):
+    for n in range(len(a)/2):
+	i = n*2-1 
+	print 'fold: ', i 
         percision = float(a[i][0])/(a[i][0] + a[i][1])
         print "percision: ", percision
         recall = float(a[i][0])/ (a[i][0] + b[i][0])
@@ -31,13 +31,15 @@ def read_results(input_text):
         correct =  a[i][0] + b[i][1]
         total = a[i][0] + a[i][1]+b[i][0]+b[i][1]
         accuracy = float(correct)/total
-
+	print accuracy
         acc.append(accuracy)
 
     return acc
 
 if __name__ == "__main__":
     a = read_results(sys.argv[1])
+    print 'length of first', len(a)
     b = read_results(sys.argv[2])
+    print 'length of second', len(b)
     S = stats.ttest_rel(a,b)
     print S
